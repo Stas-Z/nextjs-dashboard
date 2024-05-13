1a05402107c2dc4a007dfefb08796fdcb50e5ebf
Stanislav Zabolotnii <stanislav@zettafoods.ru>
2024-05-13 22:04:51 +0300

    add metadata

--- middleware.ts
+++ middleware.ts
@@ -5,5 +5,13 @@ export default NextAuth(authConfig).auth;
 
 export const config = {
   // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
+
+  /*
+   * Match all request paths except for the ones starting with:
+   * - api (API routes)
+   * - _next/static (static files)
+   * - _next/image (image optimization files)
+   * - *.png (png file)
+   */
   matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
 };
